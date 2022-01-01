6cc10aaf279ea158898c6394a41c37de95ebc174
Roland Bernard <rolli.bernard@gmail.com>
2022-01-01 22:56:09 +0100

    Solved 2015 day 4 puzzle

---
+++ 2015/4.the-ideal-stocking-stuffer/part2.py
@@ -0,0 +1,14 @@
+
+import sys
+
+from hashlib import md5
+from itertools import count
+
+magic = sys.stdin.read().strip()
+
+for i in count():
+    if md5((magic + str(i)).encode('utf-8')).hexdigest()[:6] == 6*'0':
+        break;
+
+print('Result:', i)
+
