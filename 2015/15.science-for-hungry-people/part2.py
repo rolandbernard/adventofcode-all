bc3f1d91746e51e4b809f273c65f1988175423e3
Roland Bernard <rolli.bernard@gmail.com>
2022-01-03 21:36:39 +0100

    Solved 2015 day 15 puzzle

---
+++ 2015/15.science-for-hungry-people/part2.py
@@ -0,0 +1,31 @@
+
+import sys
+
+ingr = []
+
+for inst in sys.stdin:
+    match inst.strip().split(' '):
+        case [name, 'capacity', x0, 'durability', x1, 'flavor', x2, 'texture', x3, 'calories', x4]:
+            ingr.append((int(x0[:-1]), int(x1[:-1]), int(x2[:-1]), int(x3[:-1]), int(x4)))
+
+def maxScore(i, vals, ingr):
+    if i == len(ingr) - 1:
+        vals[i] = 100 - sum(vals[:i])
+        if sum(vals[j] * ing[4] for j, ing in enumerate(ingr)) == 500:
+            return (
+                max(0, sum(vals[j] * ing[0] for j, ing in enumerate(ingr)))
+                * max(0, sum(vals[j] * ing[1] for j, ing in enumerate(ingr)))
+                * max(0, sum(vals[j] * ing[2] for j, ing in enumerate(ingr)))
+                * max(0, sum(vals[j] * ing[3] for j, ing in enumerate(ingr)))
+            )
+        else:
+            return 0
+    else:
+        best = 0
+        for a in range(100 - sum(vals[:i])):
+            vals[i] = a
+            best = max(best, maxScore(i + 1, vals, ingr))
+        return best
+
+print('Result:', maxScore(0, [0] * len(ingr), ingr))
+
