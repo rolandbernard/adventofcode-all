a50f8d96bcf1f927963f41113875d014357b1eb7
Roland Bernard <rolli.bernard@gmail.com>
2022-01-03 21:56:22 +0100

    Solved 2015 day 16 puzzle

---
+++ 2015/16.aunt-sue/part2.py
@@ -0,0 +1,39 @@
+
+import sys
+
+sues = []
+
+for inst in sys.stdin:
+    sue, *attr = (x.strip() for x in inst.split(':'))
+    attr = [a.split(':') for a in ':'.join(attr).split(',')]
+    sues.append((int(sue.split(' ')[1]), dict((a[0].strip(),int(a[1])) for a in attr)))
+
+trace = {
+    'children': 3,
+    'cats': 7,
+    'samoyeds': 2,
+    'pomeranians': 3,
+    'akitas': 0,
+    'vizslas': 0,
+    'goldfish': 5,
+    'trees': 3,
+    'cars': 2,
+    'perfumes': 1,
+}
+
+def isMatch(sue):
+    for k, v in sue[1].items():
+        if k in ['cats', 'trees']:
+            if trace[k] >= v:
+                return False
+        elif k in ['pomeranians', 'goldfish']:
+            if trace[k] <= v:
+                return False
+        elif trace[k] != v:
+            return False
+    return True
+
+posible = [s for s in sues if isMatch(s)]
+
+print('Result:', posible[0][0])
+
