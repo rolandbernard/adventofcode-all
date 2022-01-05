ccf67843d69f1d5c4d0fd60b0f2b930c419562b3
Roland Bernard <rolli.bernard@gmail.com>
2022-01-05 23:38:05 +0100

    Solved 2015 day 23 puzzle

---
+++ 2015/23.opening-the-turing-lock/part1.py
@@ -0,0 +1,32 @@
+
+import sys
+import re
+
+program = [re.split(',? ', x) for x in sys.stdin.read().strip().split('\n')]
+
+def runProgram(prog):
+    reg = { 'a': 0, 'b': 0 }
+    pc = 0
+    while pc >= 0 and pc < len(prog):
+        match prog[pc]:
+            case ['hlf', r]:
+                reg[r] //= 2
+            case ['tpl', r]:
+                reg[r] *= 3
+            case ['inc', r]:
+                reg[r] += 1
+            case ['jmp', off]:
+                pc += int(off) - 1
+            case ['jie', r, off]:
+                if reg[r] % 2 == 0:
+                    pc += int(off) - 1
+            case ['jio', r, off]:
+                if reg[r] == 1:
+                    pc += int(off) - 1
+            case instr:
+                print('Invalid instruction', instr)
+        pc += 1
+    return reg
+
+print('Result:', runProgram(program)['b'])
+
