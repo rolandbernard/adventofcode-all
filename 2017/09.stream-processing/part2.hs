2da10dba245ea858736e26c0622835d4b5ad1dfe
Roland Bernard <rolli.bernard@gmail.com>
2022-01-20 23:05:33 +0100

    Solved 2017 day 9 puzzle

---
+++ 2017/09.stream-processing/part2.hs
@@ -0,0 +1,30 @@
+
+import Debug.Trace
+
+parseGarbage _ "" = (0, "")
+parseGarbage i ('<':xs) = skipGarbage xs
+parseGarbage i ('{':xs) = (v1, r1)
+    where (v1, r1) = parseElems (i + 1) xs
+
+parseElems i s =
+    case s of
+        ('}':xs) -> (0, xs)
+        (',':xs) ->
+            let (v1, r1) = parseGarbage i xs in
+            let (v2, r2) = parseElems i r1 in
+            (v1 + v2, r2)
+        xs ->
+            let (v1, r1) = parseGarbage i xs in
+            let (v2, r2) = parseElems i r1 in
+            (v1 + v2, r2)
+
+skipGarbage ('>':xs) = (0, xs)
+skipGarbage ('!':_:xs) = skipGarbage xs
+skipGarbage (x:xs) = (v + 1, r)
+    where (v, r) = skipGarbage xs
+
+main = do
+    dat <- getContents
+    let (score, _) = parseGarbage 1 dat
+    putStrLn ("Result: " ++ show score)
+
