ecd7482340c1242d2cbe9a27ed045f52ab38a6c6
Roland Bernard <rolli.bernard@gmail.com>
2022-01-29 22:58:28 +0100

    Solved 2017 day 19 puzzle

---
+++ 2017/19.a-series-of-tubes/part1.py
@@ -0,0 +1,36 @@
+
+import sys
+
+map = [[c for c in l.strip('\n')] for l in sys.stdin]
+
+path = ''
+dir = (0, 1)
+pos = (map[0].index('|'), 0)
+
+def dirs(dir):
+    yield dir
+    if dir[0] == 0:
+        yield (1, 0)
+        yield (-1, 0)
+    else:
+        yield (0, 1)
+        yield (0, -1)
+
+def next_dir():
+    for d in dirs(dir):
+        mov = (pos[0] + d[0], pos[1] + d[1])
+        if min(mov) >= 0 and mov[1] < len(map) and mov[0] < len(map[mov[1]]) and not map[mov[1]][mov[0]].isspace():
+            return mov, d
+    return pos, None
+
+while True:
+    if map[pos[1]][pos[0]].isalpha():
+        path += map[pos[1]][pos[0]]
+    mov, dir = next_dir()
+    if pos != mov:
+        pos = mov
+    else:
+        break
+
+print('Result:', path)
+
