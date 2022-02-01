133bfd6cae333127bd5f908956a2210ac473e5a1
Roland Bernard <rolli.bernard@gmail.com>
2022-02-01 17:20:40 +0100

    Solved 2017 day 24 puzzles

---
+++ 2017/24.electromagnetic-moat/part1.rs
@@ -0,0 +1,41 @@
+
+use std::io;
+use std::io::prelude::*;
+use std::collections::*;
+
+fn longest_path(
+    cur: u64, visited: u64, nodes: &Vec<(u64, u64)>, conn: &HashMap<u64, Vec<(u64, u64)>>,
+    cache: &mut HashMap<(u64, u64), u64>
+) -> u64 {
+    if !cache.contains_key(&(cur, visited)) {
+        let mut max = 0;
+        for next in &conn[&cur] {
+            if (visited & (1 << next.1)) == 0 {
+                let dist = cur + next.0
+                    + longest_path(next.0, visited | (1 << next.1), nodes, conn, cache);
+                if dist > max {
+                    max = dist;
+                }
+            }
+        }
+        cache.insert((cur, visited), max);
+    }
+    return cache[&(cur, visited)];
+}
+
+fn main() {
+    let stdin = io::stdin();
+    let mut comps = Vec::new();
+    for line in stdin.lock().lines() {
+        let l = line.unwrap();
+        let s = l.split("/").collect::<Vec<&str>>();
+        comps.push((s[0].parse::<u64>().unwrap(), s[1].parse::<u64>().unwrap()));
+    }
+    let mut next = HashMap::new();
+    for i in 0..comps.len() {
+        (*next.entry(comps[i].0).or_insert(Vec::new())).push((comps[i].1, i as u64));
+        (*next.entry(comps[i].1).or_insert(Vec::new())).push((comps[i].0, i as u64));
+    }
+    let mut cache = HashMap::new();
+    println!("Result: {}", longest_path(0, 0, &comps, &next, &mut cache));
+}
