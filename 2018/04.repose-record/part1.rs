e01fa79ddd4826e7a9a933887968107265a028f1
Roland Bernard <rolli.bernard@gmail.com>
2022-02-04 22:37:34 +0100

    Solved 2018 day 4 puzzles

---
+++ 2018/04.repose-record/part1.rs
@@ -0,0 +1,51 @@
+
+use std::io;
+use std::io::prelude::*;
+use std::collections::*;
+
+fn main() {
+    let stdin = io::stdin();
+    let mut lines = stdin.lock().lines().map(|x| x.unwrap()).collect::<Vec<String>>();
+    lines.sort();
+    let mut guard_sleep = HashMap::new();
+    let mut guard = 0;
+    let mut start = 0;
+    for line in &lines {
+        let splt = line.split(" ").collect::<Vec<&str>>();
+        let time = splt[1][3..5].parse::<usize>().unwrap();
+        match splt[2] {
+            "Guard" => {
+                guard = splt[3][1..splt[3].len()].parse::<usize>().unwrap();
+                if !guard_sleep.contains_key(&guard) {
+                    guard_sleep.insert(guard, vec![0;60]);
+                }
+            },
+            "falls" => {
+                start = time;
+            },
+            "wakes" => {
+                for i in start..time {
+                    guard_sleep.get_mut(&guard).unwrap()[i] += 1;
+                }
+            },
+            _ => {}
+        }
+    }
+    let mut best = 0;
+    for (g, map) in guard_sleep {
+        let mut sum = 0;
+        let mut max = 0;
+        for i in 0..map.len() {
+            if map[i] > map[max] {
+                max = i;
+            }
+            sum += map[i];
+        }
+        if sum > best {
+            best = sum;
+            guard = g;
+            start = max;
+        }
+    }
+    println!("Result: {}", guard * start);
+}
