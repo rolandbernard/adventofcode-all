bdfa7e7f1fe5a7d3958ad58e950dcf021c1d50e4
Roland Bernard <rolli.bernard@gmail.com>
2022-02-09 23:56:45 +0100

    Wrote a simple intcode interpreter

---
+++ 2019/intcode/intcode.rs
@@ -0,0 +1,113 @@
+
+pub type IntValue = i64;
+pub type IntMem = Vec<IntValue>;
+
+pub fn parse(text: &str) -> IntMem {
+    return text.split(",")
+        .map(|x| x.trim().parse().unwrap())
+        .collect::<IntMem>();
+}
+
+pub struct IntState {
+    pub pc: usize,
+    pub bp: usize,
+    pub halt: bool,
+}
+
+pub fn new_state() -> IntState {
+    return IntState { pc: 0, bp: 0, halt: false };
+}
+
+fn read_param(mem: &IntMem, state: &IntState, mode: IntValue, val: IntValue) -> IntValue {
+    let off = val as usize;
+    let rel = state.bp + off;
+    return match mode {
+        0 => if off < mem.len() { mem[off] } else { 0 },
+        1 => val,
+        2 => if rel < mem.len() { mem[rel] } else { 0 },
+        _ => 0,
+    }
+}
+
+fn write_param(mem: &mut IntMem, state: &IntState, mode: IntValue, addr: IntValue, val: IntValue) {
+    if mode == 0 || mode == 2 {
+        let off;
+        if mode == 0 {
+            off = addr as usize;
+        } else {
+            off = state.bp + addr as usize;
+        }
+        if off >= mem.len() {
+            mem.resize(off + 1, 0);
+        }
+        mem[off] = val;
+    }
+}
+
+pub fn run_instr<FI, FO>(mem: &mut IntMem, state: &mut IntState, inp: FI, out: FO)
+    where FI: Fn() -> IntValue, FO: Fn(IntValue)
+{
+    if !state.halt {
+        let instr = mem[state.pc];
+        let op = instr % 100;
+        let pm0 = (instr / 100) % 10;
+        let pm1 = (instr / 1000) % 10;
+        let pm2 = (instr / 10000) % 10;
+        match op {
+            1 => {
+                let v0 = read_param(mem, state, pm0, mem[state.pc + 1]);
+                let v1 = read_param(mem, state, pm1, mem[state.pc + 2]);
+                write_param(mem, state, pm2, mem[state.pc + 3], v0 + v1);
+                state.pc += 4;
+            },
+            2 => {
+                let v0 = read_param(mem, state, pm0, mem[state.pc + 1]);
+                let v1 = read_param(mem, state, pm1, mem[state.pc + 2]);
+                write_param(mem, state, pm2, mem[state.pc + 3], v0 * v1);
+                state.pc += 4;
+            },
+            3 => {
+                write_param(mem, state, pm0, mem[state.pc + 1], inp());
+                state.pc += 2;
+            },
+            4 => {
+                out(read_param(mem, state, pm0, mem[state.pc + 1]));
+                state.pc += 2;
+            },
+            5 => {
+                if read_param(mem, state, pm0, mem[state.pc + 1]) != 0 {
+                    state.pc = read_param(mem, state, pm1, mem[state.pc + 2]) as usize;
+                } else {
+                    state.pc += 3;
+                }
+            }
+            6 => {
+                if read_param(mem, state, pm0, mem[state.pc + 1]) == 0 {
+                    state.pc = read_param(mem, state, pm1, mem[state.pc + 2]) as usize;
+                } else {
+                    state.pc += 3;
+                }
+            }
+            7 => {
+                let v0 = read_param(mem, state, pm0, mem[state.pc + 1]);
+                let v1 = read_param(mem, state, pm1, mem[state.pc + 2]);
+                write_param(mem, state, pm2, mem[state.pc + 3], if v0 < v1 { 1 } else { 0 });
+                state.pc += 4;
+            },
+            8 => {
+                let v0 = read_param(mem, state, pm0, mem[state.pc + 1]);
+                let v1 = read_param(mem, state, pm1, mem[state.pc + 2]);
+                write_param(mem, state, pm2, mem[state.pc + 3], if v0 == v1 { 1 } else { 0 });
+                state.pc += 4;
+            },
+            9 => {
+                state.bp += read_param(mem, state, pm0, mem[state.pc + 1]) as usize;
+                state.pc += 2;
+            },
+            _ => {
+                state.halt = true;
+                state.pc += 1;
+            },
+        }
+    }
+}
---
+++ 2019/intcode/main.rs
@@ -0,0 +1,30 @@
+
+use std::fs;
+use std::io;
+use std::env;
+use std::io::Read;
+use std::io::Write;
+
+mod intcode;
+
+fn main() {
+    let stdin = io::stdin();
+    let stdout = io::stdout();
+    let args = env::args().collect::<Vec<String>>();
+    let code = fs::read_to_string(&args[1]).unwrap();
+    let mut mem = intcode::parse(&code);
+    let mut cpu = intcode::new_state();
+    while !cpu.halt {
+        let inp = || {
+            return stdin.lock().bytes().next()
+                .and_then(|result| result.ok())
+                .map(|byte| byte as intcode::IntValue)
+                .unwrap_or(0);
+        };
+        let out = |x| {
+            stdout.lock().write_all(&[x as u8]).unwrap();
+        };
+        intcode::run_instr(&mut mem, &mut cpu, inp, out);
+    }
+}
+
